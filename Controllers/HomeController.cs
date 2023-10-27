b4e4dd62c714fe66ddf0ecf0df04874e95c98c9c
512kbb <dieg.parraa@duocuc.cl>
2023-10-27 20:27:12 -0300

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DojoSurvey.Models;
+
+namespace DojoSurvey.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost("process")]
+    public RedirectToActionResult Process(string Name, int Location, int FavoriteLanguage, string Comment)
+    {
+        string selectedLocation = "";
+        string selectedLanguage = "";
+        if (Location == 0)
+        {
+            selectedLocation = "San Jose";
+        } else 
+        {
+            selectedLocation = "Santiago";
+        }
+
+        if (FavoriteLanguage == 0)
+        {
+            selectedLanguage = "C#";
+        } else if (FavoriteLanguage == 1)
+        {
+            selectedLanguage = "Javascript";
+        } else
+        {
+            selectedLanguage =  "Python";
+        }
+
+        return RedirectToAction("Result", new { 
+            name = Name,
+            location = selectedLocation,
+            favoriteLanguage = selectedLanguage,
+            comment = Comment
+        });
+    }
+
+    public IActionResult Result(string name, string location, string favoriteLanguage, string comment)
+    {
+        Console.WriteLine(name);
+        ViewBag.Name = name;
+        ViewBag.Location = location;
+        ViewBag.favoriteLanguage = favoriteLanguage;
+        ViewBag.Comment = comment;
+        return View("Result");
+    }
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
